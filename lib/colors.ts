a2a3eef22e0ec899f3c31b7124ba0eda3f9e96b5
Nathan Brodin <nathan@brodin.dev>
2025-08-07 16:57:22 +0200

    Remove amber and rose color themes

--- lib/colors.ts
+++ lib/colors.ts
@@ -15,22 +15,6 @@ export const baseColors = [
       dark: "oklch(0.59 0.2 355.89)",
     },
   },
-  {
-    name: "amber",
-    label: "Amber",
-    activeColor: {
-      light: "oklch(0.67 0.11 118.91)",
-      dark: "oklch(0.68 0.06 132.45)",
-    },
-  },
-  {
-    name: "rose",
-    label: "Rose",
-    activeColor: {
-      light: "oklch(0.56 0.13 43.00)",
-      dark: "oklch(0.56 0.13 43.00)",
-    },
-  },
 ] as const
 
 export type BaseColor = (typeof baseColors)[number]
